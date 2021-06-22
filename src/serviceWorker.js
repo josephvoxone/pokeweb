7d62fad9f8a6b016cf43937d93aaa04ba77f6a08
josephvoxone <josephshandyharvian@gmail.com>
2021-06-22 23:41:19 +0700

    Update Service

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
@@ -94,7 +94,7 @@ function registerValidSW(swUrl, config) {
             };
         })
         .catch(error => {
-            // console.log('Error during service worker registration: server doesnt support', error);
+            console.log('Error during service worker registration: ', error);
         });
 }
 
